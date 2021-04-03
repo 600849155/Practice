c0d9f30b57c939d05da8de766c1ffcd41f7d9126
whohim <534801304@qq.com>
2021-04-03 22:47:51 +0800

    feat: add SimpleRateLimiter

---
+++ src/main/java/com/redis/SimpleRateLimiter.java
@@ -0,0 +1,44 @@
+package com.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Response;
+
+import java.io.IOException;
+
+/**
+ * @author WhomHim
+ * @description
+ * @date Create in 2021/4/3 22:18
+ */
+public class SimpleRateLimiter {
+
+    private Jedis jedis;
+
+    public SimpleRateLimiter(Jedis jedis) {
+        this.jedis = jedis;
+    }
+
+    public boolean isActionAllowed(String userId, String actionKey, int period, int maxCount) throws IOException {
+        String key = String.format("hist:%s:%s", userId, actionKey);
+        long nowTs = System.currentTimeMillis();
+        Pipeline pipe = jedis.pipelined();
+        pipe.multi();
+        pipe.zadd(key, nowTs, "" + nowTs);
+        pipe.zremrangeByScore(key, 0, nowTs - period * 1000);
+        Response<Long> count = pipe.zcard(key);
+        pipe.expire(key, period + 1);
+        pipe.exec();
+        pipe.close();
+        return count.get() <= maxCount;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Jedis jedis = new Jedis("localhost");
+        SimpleRateLimiter limiter = new SimpleRateLimiter(jedis);
+        for (int i = 0; i < 20; i++) {
+            System.out.println(limiter.isActionAllowed("laoqian", "reply", 60, 5));
+        }
+    }
+}
+
