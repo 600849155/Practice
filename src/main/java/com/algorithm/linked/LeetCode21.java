332776d2b2ba374bdda1197890f05189b20a5f19
WhoHim <534801304@qq.com>
2021-10-28 17:45:10 +0800

    fix: fix problem

--- src/main/java/com/algorithm/linked/LeetCode21.java
+++ src/main/java/com/algorithm/linked/LeetCode21.java
@@ -7,42 +7,40 @@ package com.algorithm.linked;
  */
 public class LeetCode21 {
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode();
-        ListNode curHead;
-        ListNode curRed = l1;
-        ListNode curBlue = l2;
-        while (curRed != null && curBlue != null) {
-            ListNode redTemp = curRed.next;
-            ListNode blueTemp = curBlue.next;
-            if (curRed.val >= curBlue.val) {
-                head.next = curBlue;
-                head.next.next = curRed;
-            } else {
-                head.next = curRed;
-                head.next.next = curBlue;
-            }
-            curHead = head.next.next;
-            head = curHead;
-            curBlue = blueTemp;
-            curRed = redTemp;
+        ListNode preHead = new ListNode(-1);
 
+        ListNode pre = preHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                pre.next = l1;
+                l1 = l1.next;
+                pre = pre.next;
+            } else {
+                pre.next = l2;
+                l2 = l2.next;
+                pre = pre.next;
+            }
         }
-        return head;
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        pre.next = l1 == null ? l2 : l1;
+
+        return preHead.next;
     }
 
     public static void main(String[] args) {
         ListNode red1 = new ListNode(1);
         ListNode red2 = new ListNode(2);
-        ListNode red3 = new ListNode(4);
+        ListNode red3 = new ListNode(3);
         red1.next = red2;
         red2.next = red3;
 
-        ListNode blue1 = new ListNode(1);
-        ListNode blue2 = new ListNode(3);
-        ListNode blue3 = new ListNode(4);
+        ListNode blue1 = new ListNode(4);
+        ListNode blue2 = new ListNode(5);
+        ListNode blue3 = new ListNode(6);
         blue1.next = blue2;
         blue2.next = blue3;
-        final ListNode listNode = mergeTwoLists(red1, blue2);
+        final ListNode listNode = mergeTwoLists(red1, blue1);
         System.out.println(listNode);
     }
 }
